d819ba60814e99d9100c856f5340695de505ba41
Puppy <1312058405@qq.com>
2021-03-05 17:29:01 +0800

    创建wordcount程序

---
+++ hadoop-module/src/main/java/org/puppy/hadoop/app/HadoopWordCountApplication.java
@@ -0,0 +1,14 @@
+package org.puppy.hadoop.app;
+
+/**
+ * 只利用hadoop实现word count程序
+ *
+ * @author puppy(陶江航 taojianghangdsjb @ smegz.cn)
+ * @since 2021/3/5 16:38
+ */
+public class HadoopWordCountApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
